cbafc161c86ed71ca9795c9eef4a3ef64ea67bce
jonathandodard <jonathan.dodard@gmail.com>
2017-07-11 09:42:30 +0200

    add composer on install

--- script_install.sh
+++ script_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Indique au système que l'argument qui suit est le programme utilisé pour exécuter ce fichier
 # En règle générale, les "#" servent à mettre en commentaire le texte qui suit comme ici
-sudo apt-get update
 sudo apt-get install unity-tweak-tool -y
 sudo apt-get install gdebi -y
 sudo apt-get install plank -y
@@ -9,7 +8,6 @@ sudo apt-get install php -y
 sudo apt-get install php-mysql -y
 sudo apt-get install php-cli -y
 sudo apt-get install phpunit -y
-sudo apt-get update
 sudo apt-get install npm -y
 sudo apt-get install wget -y
 sudo apt-get install curl -y
@@ -19,7 +17,7 @@ sudo apt-get install tig -y
 sudo apt-get install terminator -y
 sudo apt-get install vim -y
 sudo apt-get install powerline -y
-sudo apt-get update
+sudo apt-get install composer -y
 sudo mkdir -p /usr/local/bin
 sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
 sudo chmod a+x /usr/local/bin/symfony
